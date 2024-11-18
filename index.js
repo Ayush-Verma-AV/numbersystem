bd88bf1b1e0513ee0a5e91ccf4ee7551fd88bd01
Ayush-Verma-AV <vvayush490@gmail.com>
2024-11-18 17:49:38 +0530

    We added Even And Odd in single code

---
+++ index.js
@@ -0,0 +1,18 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+  let n = numbers.length;
+  let sum = 0;
+
+  for(let i=0; i<n; i++){
+    if(numbers[i]%2 ==0)
+  
+console.log("even",numbers[i])
+  }
+
+  
+  for(let i=0; i<n; i++){
+    if(numbers[i]%2 !=0)
+  
+console.log("Odd", numbers[i])
+  }
